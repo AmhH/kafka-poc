a5da463b26ad274632323e1742779ee6057a0782
Amh <abaams78@gmail.com>
2020-03-18 14:49:32 -0500

    test

---
+++ src/test/java/com/example/KafkaListenerApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class KafkaListenerApplicationTests {
+
+/*	@Test
+	void contextLoads() {
+	}*/
+
+	@Test
+	public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+	}
+
+}
